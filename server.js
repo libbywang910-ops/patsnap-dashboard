7d81d64275055452da18d19ead8c4da6ca7f6192
libbywang910-ops <libbywang910@gmail.com>
2026-05-18 00:09:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Patsnap Dashboard running on port ${PORT}`);
 });
